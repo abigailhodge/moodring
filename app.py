27d993d96f7290290c1fff2fe986fbb730fc9cce
Vanessa Hu <vanessahu248@gmail.com>
2020-01-31 23:53:54 -0500

    added dependencies

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"]
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    hi = "hello"
+    return render_template('index.html', hi=hi)
 
 if __name__ == "__main__":
     app.run()
