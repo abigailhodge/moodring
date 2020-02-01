c42a90a297ea32cf731610f2cf8aef18e37aab60
Vanessa Hu <vanessahu248@gmail.com>
2020-02-01 09:13:22 -0500

    starting mongo db

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, redirect, request, session
 from datetime import datetime, timedelta
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import os
 
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"]
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/db"
 mongo = PyMongo(app)
+client = MongoClient("mongodb://127.0.0.1:27017")
 
 
 @app.route("/")
@@ -26,4 +29,7 @@ def add_entry():
         return render_template("addentry.html")
     else:
         journal = request.form.get("journal")
-        return render_template("index.html")
+        year=(datetime.now() - timedelta(hours=5)).year
+        month=(datetime.now() - timedelta(hours=5)).month
+        day=(datetime.now() - timedelta(hours=5)).month
+        return render_template("index.html", month=month, day=day, year=year)
